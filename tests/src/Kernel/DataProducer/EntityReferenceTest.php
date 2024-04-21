ea48879d68da2a2a490797fd5369924741a9b999
Klaus Purer <klaus.purer@protonmail.ch>
2024-04-21 11:44:28 +0200

    revert EntityReferenceTest

--- tests/src/Kernel/DataProducer/EntityReferenceTest.php
+++ tests/src/Kernel/DataProducer/EntityReferenceTest.php
@@ -5,8 +5,32 @@ namespace Drupal\Tests\graphql\Kernel\DataProducer;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
-use Drupal\Tests\field\Traits\EntityReferenceFieldCreationTrait;
 use Drupal\Tests\graphql\Kernel\GraphQLTestBase;
+
+// @todo Drupal 10.1 compatibility: use the deprecated trait for Drupal 10.1.
+if (strpos(\Drupal::VERSION, '10.1') === 0) {
+
+  /**
+   * Helper trait for compatibility with Drupal 9.
+   *
+   * @phpcs:disable Drupal.Classes.ClassFileName.NoMatch
+   */
+  trait EntityReferenceFieldCreationTrait {
+    // @phpstan-ignore-next-line
+    use \Drupal\Tests\field\Traits\EntityReferenceTestTrait;
+
+  }
+}
+else {
+
+  /**
+   * Helper trait for compatibility with Drupal 10.
+   */
+  trait EntityReferenceFieldCreationTrait {
+    use \Drupal\Tests\field\Traits\EntityReferenceFieldCreationTrait;
+
+  }
+}
 
 /**
  * Tests the entity_reference data producers.
